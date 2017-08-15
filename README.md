2b5284a61644e17afa15af6d24f620551d3d7c86
guankun <mu.g@husky.neu.edu>
2017-08-15 14:51:35 -0700

    Created node server.

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS5610-Project-StarGazers
+
+CS5610 Web Development 2017 Summer 
+
+Name: Guankun Mu 
+
+Project Link: https://CS5610-Project-StarGazers.herokuapp.com
+
+GitHub Repo: https://github.com/guankun/CS5610-Project-StarGazers
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'this is the secret',
+    resave: true,
+    saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./public/server.js")(app);
+
+var port = process.env.PORT || 3001;
+app.listen(port);
+
+console.log("Application start at port " + port);
